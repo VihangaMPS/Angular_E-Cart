3ab94f7de35e6fc70274d0a8aae21b0f69412ead
MPS Vihanha <>
2024-02-09 14:59:30 +0530

    v2.0 Header Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './header/top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { BottomHeaderComponent } from './header/bottom-header/bottom-header.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { TopMenuComponent } from './header/top-menu/top-menu.component';
     AppComponent,
     HeaderComponent,
     TopHeaderComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    MainMenuComponent,
+    BottomHeaderComponent
   ],
   imports: [
     BrowserModule,
